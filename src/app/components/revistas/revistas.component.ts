8e7f79be9413753d93d5ecb679101625e0ecaf60
Shandel <sortiz-2019186@kinal.edu.gt>
2021-11-10 23:29:40 -0600

    CRUD de revistas

--- src/app/components/revistas/revistas.component.ts
+++ src/app/components/revistas/revistas.component.ts
@@ -1,15 +1,140 @@
 import { Component, OnInit } from '@angular/core';
+import { Revista } from 'src/app/models/revista.model';
+import { RevistaService } from 'src/app/services/revista.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-revistas',
   templateUrl: './revistas.component.html',
-  styleUrls: ['./revistas.component.scss']
+  styleUrls: ['./revistas.component.scss'],
+  providers: [RevistaService, UsuarioService]
 })
 export class RevistasComponent implements OnInit {
+  public token: String;
+  public revistasList: any;
+  public revistaIDModel: Revista;
+  public revistaModel: Revista;
 
-  constructor() { }
+  constructor(private revistaService: RevistaService, private usuarioService: UsuarioService) {
+    this.token = this.usuarioService.getToken();
+    this.revistaIDModel = new Revista('', '', '', 0, '', '', 0, [], [], 0, 0);
+    this.revistaModel = new Revista('', '', '', 0, '', '', 0, [], [], 0, 0);
+  }
 
   ngOnInit(): void {
+    this.obtenerRevistas();
+  }
+
+  crearRevista() {
+    this.revistaService.crearRevista(this.revistaModel, this.token).subscribe(
+      response => {
+        console.log(response);
+
+        this.obtenerRevistas();
+
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: '¡Revista creada y guardada con éxito!',
+          showConfirmButton: false,
+          timer: 3000
+        });
+      },
+      error => {
+        console.log(<any>error);
+
+        this.obtenerRevistas();
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.error.message
+        });
+      }
+    );
+  }
+
+  obtenerRevistas() {
+    this.revistaService.obtenerRevistas(this.token).subscribe(
+      response => {
+        console.log(response.revistasEncontradas);
+        this.revistasList = response.revistasEncontradas;
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  obtenerRevista(id: any) {
+    this.revistaService.obtenerRevista(id, this.token).subscribe(
+      response => {
+        console.log(response.revistaEncontrada);
+        this.revistaIDModel = response.revistaEncontrada;
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  modificarRevista() {
+    this.revistaService.modificarRevista(this.revistaIDModel, this.token).subscribe(
+      response => {
+        console.log(response);
+
+        this.obtenerRevistas();
+
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: '¡Revista editada y guardada con éxito!',
+          showConfirmButton: false,
+          timer: 3000
+        });
+      },
+      error => {
+        console.log(<any>error);
+
+        this.obtenerRevistas();
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.error.message
+        });
+      }
+    );
+  }
+
+  eliminarRevista(id: any) {
+    this.revistaService.eliminarRevista(id, this.token).subscribe(
+      response => {
+        console.log(response);
+
+        this.obtenerRevistas();
+
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: '¡Revista eliminada con éxito!',
+          showConfirmButton: false,
+          timer: 3000
+        });
+      },
+      error => {
+        console.log(<any>error);
+
+        this.obtenerRevistas();
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.error.message
+        });
+      }
+    );
   }
 
 }
--- src/app/services/revista.service.ts
+++ src/app/services/revista.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GLOBAL } from './global.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Revista } from '../models/revista.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RevistaService {
+  public ruta: String;
+  public headersVariable = new HttpHeaders().set('Content-Type', 'application/json');
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    this.ruta = GLOBAL.url;
+  }
+
+  crearRevista(revista: Revista, token: any): Observable<any> {
+    let body = JSON.stringify(revista);
+    let headersToken = this.headersVariable.set('token', token);
+
+    return this.http.post(`${ this.ruta }crearRevista`, body, { headers: headersToken });
+  }
+
+  obtenerRevistas(token: any): Observable<any> {
+    let  headersToken = this.headersVariable.set('token', token);
+
+    return this.http.get(`${ this.ruta }obtenerRevistas`, { headers: headersToken });
+  }
+
+  obtenerRevista(id: String, token: any): Observable<any> {
+    let headersToken = this.headersVariable.set('token', token);
+
+    return this.http.get(`${ this.ruta }obtenerRevista/${ id }`, { headers: headersToken });
+  }
+
+  modificarRevista(revista: Revista, token: any): Observable<any> {
+    let body = JSON.stringify(revista);
+    let headersToken = this.headersVariable.set('token', token);
+
+    return this.http.put(`${ this.ruta }modificarRevista/${ revista._id }`, body, { headers: headersToken });
+  }
+
+  eliminarRevista(id: String, token: any) {
+    let headersToken = this.headersVariable.set('token', token);
+
+    return this.http.delete(`${ this.ruta }eliminarRevista/${ id }`, { headers: headersToken });
+  }
 }
